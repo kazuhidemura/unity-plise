313f198f6e47f1977b02ea91a32ccd30e06cb797
kazuhidemura <kazuhidemura2@icloud.com>
2019-10-24 17:57:31 +0900

    20191024 ボール遊び

---
+++ Roll a Ball/Assets/Scriptss/seigyo1p.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class seigyo1p : MonoBehaviour
+{
+    public float speed = 20;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      var moveHorizontal = Input.GetAxis("Horizontal");
+      var moveVertical = Input.GetAxis("Vertical");
+      var movememt = new Vector3(moveHorizontal, 0, moveVertical);
+      rb.AddForce(movememt * speed);
+       
+    }
+}
