ff280b81ba760c5cc451b9afc00fa328d0363e0a
kazuhidemura <kazuhidemura2@icloud.com>
2019-10-31 18:14:34 +0900

    シューティングゲーム

---
+++ シューティングゲーム/Assets/Scripts/P1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class P1 : MonoBehaviour
+{
+    public float m_speed;
+    // Start is called before the first frame update
+    private void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        var velocity = new Vector3(h, v) * m_speed;
+        transform.localPosition += velocity;
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
